b13891338c1522ca106ec987f216433eef541f3a
johnnybro360 <abest82@gmail.com>
2023-02-08 13:52:51 +0800

    add user input

--- password_generator.py
+++ password_generator.py
@@ -1,9 +1,19 @@
 import random
 import string
 
-def generate_password():
+def generate_password(length):
     chars = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(chars) for i in range(10))
+    password = ''.join(random.choice(chars) for i in range(length))
     return password
 
-print("Generated password:", generate_password())
+while True:
+    try:
+        length = int(input("Enter the password length (1-50): "))
+        if length > 0 and length <= 50:
+            break
+        else:
+            print("Invalid length. Please try again.")
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+
+print("Generated password:", generate_password(length))
